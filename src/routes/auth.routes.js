8a7086bd429793467e069ee7431afeb773ab27ca
MarcoSavarin0 <ateneo2003@gmail.com>
2024-03-02 20:41:42 -0300

    pongo de nuevo el middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/auth.controller')
 const authJwt = require('../middleware/authJwt')
-router.post('/signup', controller.signUp)
+router.post('/signup', authJwt,controller.signUp)
 router.post('/signin', controller.signIn)
 
 
